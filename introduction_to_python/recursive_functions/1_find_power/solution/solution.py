26d9c9ce864b5065d1fce746abb0b1a3bc1f7046
Yuta Hagiwara <yhagiwara@outlook.com>
2021-04-26 15:31:11 -0400

    modify code

--- introduction_to_python/recursive_functions/1_find_power/solution/solution.py
+++ introduction_to_python/recursive_functions/1_find_power/solution/solution.py
@@ -12,6 +12,8 @@
 def power(a,b):
     if b==0:
         return 1
-    else:
-        return a*power(a, b-1)
+    if b==1:
+        return a
+    return a* power(a,b-1)
+
 print(power(2,3))
--- introduction_to_python/recursive_functions/3_factorial/solution/solution.py
+++ introduction_to_python/recursive_functions/3_factorial/solution/solution.py
@@ -12,7 +12,5 @@
 def factorial_recursive(n):
     if n==0:
         return 1
-    else:
-        return n*factorial_recursive(n-1)
-    
+    return n*factorial_recursive(n-1)
 print(factorial_recursive(4))
