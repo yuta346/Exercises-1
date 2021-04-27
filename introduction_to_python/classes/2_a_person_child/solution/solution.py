17796820033e6a2bf800af8d12864dd0c53c9b8e
Yuta Hagiwara <yhagiwara@outlook.com>
2021-04-26 23:22:48 -0400

    initial commit

--- introduction_to_python/classes/2_a_person_child/solution/solution.py
+++ introduction_to_python/classes/2_a_person_child/solution/solution.py
@@ -1,3 +1,5 @@
+#Completed
+
 ## Problem Description
 # Define a Python class Person. The constructor function should take in the following attributes:
 # * `name` - a string
@@ -17,3 +19,22 @@
 # `suzy` has the name `Suzy Brown`, an age of `42`, with `jim` as her spouse, and one child `martha`.
 # Define a child object assined to the variable `martha` with the name `Martha Brown`, 
 # aged `18`, having no spouse and no children, and having parents `jim` and `suzy`.
+
+class Person:
+    def __init__(self,name,age,spouse,children):
+        self.name = name
+        self.age = age
+        self.spouse = spouse
+        self.children = children
+
+class children(Person):
+    def __init__(self,name,age,spouse,children,parents):
+        Person.__init__(self,name,age,spouse,children)
+        self.parents = parents
+
+jim = Person('Jim Brown',45,None,[])
+suzy = Person('Suzy Brown',42,jim,[])
+jim.spouse = suzy
+martha = children('Martha Brown',18,None,[],[jim,suzy])
+jim.children.append(martha)
+suzy.children.append(martha)
